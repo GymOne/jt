1780cfe4572f7a25500640cf90e3fa8a814583bb
Montekar <mantas.pasakarnis@gmail.com>
2022-05-13 13:45:14 +0200

    ex

--- backend-trial/dist/src/infrastructure/mongo/workouts.provider.d.ts
+++
@@ -1,6 +0,0 @@
-import { Connection } from 'mongoose';
-export declare const WorkoutsProvider: {
-    provide: string;
-    useFactory: (connection: Connection) => import("mongoose").Model<any, any, any, any>;
-    inject: string[];
-}[];
